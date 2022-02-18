54d4d1b1bfb8c01f3d4a7d09003dbf4692b27680
Ytin24 <lepaew228@gmail.com>
2022-02-18 23:59:11 +0300

    реализовал возможность рассылки в нескольких группах

--- SVKGroupSender/Program.cs
+++ SVKGroupSender/Program.cs
@@ -14,12 +14,12 @@ namespace SVKGroupSender
     }
     class Interface
     {
-        static string token;
+        static string[] token;
         static string text;
         //private static Dictionary<string, Data> Information = new Dictionary<string, Data>();
         public static void Menu()
         {
-            if (File.Exists(Directory.GetCurrentDirectory() + "//token.txt")) token = File.ReadAllText(Directory.GetCurrentDirectory() + "//token.txt");
+            if (File.Exists(Directory.GetCurrentDirectory() + "//token.txt")) token = File.ReadAllLines(Directory.GetCurrentDirectory() + "//token.txt");
             Console.Clear();
             Console.Title = "SetFps SVKGroupSender Modern by Ytin24";
             Console.WriteLine("SetFps SVKGroupSender - 0.1 by Ytin24");
@@ -33,7 +33,7 @@ namespace SVKGroupSender
                     StartGroupSend();
                     break;
                 case 2:
-                    Process.Start("Explorer.exe","https://vk.com/setfpsnepidor");
+                    Process.Start("Explorer.exe", "https://vk.com/setfpsnepidor");
                     Menu();
                     break;
                 case 3:
@@ -75,48 +75,53 @@ namespace SVKGroupSender
             {
                 if (text != null && token != null)
                 {
-                    Console.WriteLine($"Токен: {token}");
                     Console.WriteLine($"Текст: {text}");
                     Thread.Sleep(1000);
                     RestClient client = new RestClient();
-                    RestRequest request = new RestRequest($"https://api.vk.com/method/messages.getConversations?access_token={token}&v=5.130&count=1");
-                    var response = client.GetAsync(request).Result;
-                    var Group = JsonConvert.DeserializeObject<BasicResponse<ItemsResponse<ConversationResponse>>>(response.Content);
-                    int testint = 1;
-                    var testtime = DateTime.Now.ToShortTimeString();
-                    Console.WriteLine("Сейчас - " + testtime.ToString());
-                    for (int a = 0; a < Group.Response.Count; a+=100)
+                    for (int n = 0; n < token.Length; n++)
                     {
-                        request = new RestRequest($"https://api.vk.com/method/messages.getConversations?access_token={token}&v=5.130&count=100&offset={a}");
-                        response = client.GetAsync(request).Result;
-                        Group = null;
-                        Group = JsonConvert.DeserializeObject<BasicResponse<ItemsResponse<ConversationResponse>>>(response.Content);
-                        if (Group.Response != null)
+                        Console.WriteLine($"Токен: {token[n]}");
+                        RestRequest request = new RestRequest($"https://api.vk.com/method/messages.getConversations?access_token={token[n]}&v=5.130&count=1");
+                        var response = client.GetAsync(request).Result;
+                        var Group = JsonConvert.DeserializeObject<BasicResponse<ItemsResponse<ConversationResponse>>>(response.Content);
+                        int testint = 1;
+                        var testtime = DateTime.Now.ToShortTimeString();
+                        Console.WriteLine("Сейчас - " + testtime.ToString());
+                        for (int a = 0; a < Group.Response.Count; a += 100)
                         {
-
-                            for (int i = 0; i < Group.Response.Items.Count; i++)
+                            request = new RestRequest($"https://api.vk.com/method/messages.getConversations?access_token={token[n]}&v=5.130&count=100&offset={a}");
+                            response = client.GetAsync(request).Result;
+                            Group = null;
+                            Group = JsonConvert.DeserializeObject<BasicResponse<ItemsResponse<ConversationResponse>>>(response.Content);
+                            if (Group.Response != null)
                             {
-                                
-                                try
+
+                                for (int i = 0; i < Group.Response.Items.Count; i++)
                                 {
-                                    if (Group.Response.Items[i].Conversation.WriteSettings.Allowed && Group.Response.Items[i].Conversation.Peer.Type != "chat")
+
+                                    try
                                     {
-                                        request = new RestRequest($"https://api.vk.com/method/messages.send?access_token={token}&peer_id={Group.Response.Items[i].Conversation.Peer.Id}&random_id=0&message={text}&v=5.130");
-                                        var send = client.GetAsync(request);
-                                        Console.WriteLine("Отправил сообщение пользователю {1} \t {2}",Group.Response.Items[i].Conversation.Peer.Id, testint++);
-                                        //Console.WriteLine("тест {0} {1} \t {2} \t {3}", Group.Response.Items[i].Conversation.WriteSettings.Allowed, Group.Response.Items[i].Conversation.Peer.Id,testint++, Group.Response.Items[i].Conversation.Peer.Type); //тест отправка*
-                                        Thread.Sleep(TimeSpan.FromSeconds(0.5));
+                                        if (Group.Response.Items[i].Conversation.WriteSettings.Allowed && Group.Response.Items[i].Conversation.Peer.Type != "chat")
+                                        {
+                                            //request = new RestRequest($"https://api.vk.com/method/messages.send?access_token={token[n]}&peer_id={Group.Response.Items[i].Conversation.Peer.Id}&random_id=0&message={text}&v=5.130");
+                                            //var send = client.GetAsync(request);
+                                            //Console.WriteLine("Отправил сообщение пользователю {1} \t {2}",Group.Response.Items[i].Conversation.Peer.Id, testint++);
+                                            Console.WriteLine("тест {0} {1} \t {2} \t {3}", Group.Response.Items[i].Conversation.WriteSettings.Allowed, Group.Response.Items[i].Conversation.Peer.Id, testint++, Group.Response.Items[i].Conversation.Peer.Type); //тест отправка*
+                                            Thread.Sleep(TimeSpan.FromSeconds(0.5));
+                                        }
                                     }
+                                    catch (Exception ex) { break; }
                                 }
-                                catch (Exception ex) { break; }
                             }
+                            else { Console.WriteLine(Group.Error.Message); Console.ReadLine(); }
                         }
-                        else { Console.WriteLine(Group.Error.Message); Console.ReadLine(); }
+
                     }
-                    Console.WriteLine("Готово!  {0}",DateTime.Now.ToShortTimeString());
+                    Console.WriteLine("Готово!  {0}", DateTime.Now.ToShortTimeString());
                     Console.ReadLine();
                     Menu();
                 }
+
                 else if (text == null)
                 {
                     ChangeTextToSend();
@@ -136,15 +141,15 @@ namespace SVKGroupSender
         {
             if (token != null)
             {
-                if (File.Exists(Directory.GetCurrentDirectory() + "//token.txt")) File.Create(Directory.GetCurrentDirectory() + "token.txt");
-                File.WriteAllText(Directory.GetCurrentDirectory() + "//token.txt", token);
-
+                if (!File.Exists(Directory.GetCurrentDirectory() + "//token.txt")) File.Create(Directory.GetCurrentDirectory() + "token.txt");
+                File.WriteAllLines(Directory.GetCurrentDirectory() + "//token.txt", token);
                 StartGroupSend();
             }
             else
             {
-                Console.WriteLine(("Введи токен группы *он будет сохранен, чтобы его изменить удалите файл token.txt в папке с программой*"));
-                token = Console.ReadLine();
+                Console.WriteLine(("Введи токен группы\n*он будет сохранен, чтобы его изменить удалите файл token.txt в папке с программой*\n(если их несколько, то разделять запятой)"));
+                string tokenstr = Console.ReadLine();
+                token = tokenstr.Split(',');
                 AddGroupToken();
 
             }
